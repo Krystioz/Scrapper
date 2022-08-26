23b3e2d7ffd3a0596ed9f07a7c94b06884b01422
Krystian Głuszko <gluszkokrystian@gmail.com>
2022-08-26 19:29:24 +0200

    Finalising the result, logging groupped sorted

--- index.mjs
+++ index.mjs
@@ -8,90 +8,39 @@ const vgmUrl = "https://justjoin.it/api/offers";
 got(vgmUrl)
 	.then((result) => {
 		const parsed = JSON.parse(result.body);
-		let jobTitles = handleResult(parsed);
-		let titles = compareJobTitles(jobTitles);
-		let sortedTitles = titles.sort((a, b) => b.length - a.length);
-		writeTitles(sortedTitles);
-		// console.log(rawdata.split(","))
-		// writeOffersFile(parsed);
+		let jobTitles = getTitlesArr(parsed);
+		let sortedTitles = jobTitles.sort((a, b) => b.count - a.count);
+		let grouppedTitles = getCompared(sortedTitles);
+		let sortedGrouppedTitles = grouppedTitles.sort((a, b) => b.score - a.score);
+		//deleting similar arr to make console.log() clearer to read
+		_.each(sortedGrouppedTitles, (e) => delete e.similar);
+		console.log(sortedGrouppedTitles);
 	})
 	.catch((err) => {
 		console.log(err);
 	});
 
-function handleResult(result) {
-	const parsed = result;
-	const titles = getTitles(parsed);
-	return titles;
-}
-
-function getTitles(res) {
+function getTitlesArr(res) {
 	const mappedTitles = _.map(res, (e, i) => e.title);
-	const occurences = _.reduce(
-		mappedTitles,
-		function (acc, curr) {
-			return acc[curr] ? ++acc[curr] : (acc[curr] = 1), acc;
-		},
-		{}
+	const occurences = _.countBy(mappedTitles, (e) =>
+		e
+			.toLowerCase()
+			.replace(
+				/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+				""
+			)
+			.replace("-", "")
 	);
 	const arrOfArr = Object.entries(occurences);
-	return transformToObjArr(arrOfArr);
+	return transformTitlesToObjArr(arrOfArr);
 }
 
-function transformToObjArr(data) {
+function transformTitlesToObjArr(data) {
 	const arr = [];
 	data.forEach(function (el) {
 		arr.push({ name: el[0], count: el[1] });
 	});
 	return arr;
-}
-
-function writeOffersFile(res) {
-	fs.writeFile(
-		`offer_files/justJoinData${Date.now()}.json`,
-		`${JSON.stringify(res)}`,
-		(err) => console.log(err)
-	);
-}
-
-function writeTitles(res) {
-	fs.writeFile(
-		`titles_files/titles_sorted${floorRandom(1000)}.json`,
-		`${JSON.stringify(res)}`,
-		(err) => console.log(err)
-	);
-}
-
-function compareJobTitles(titles) {
-	let titleObjects = [];
-
-	titles.forEach(function (el, indx) {
-		let isMatch = false;
-		let nameObj = [];
-		let name = el.name;
-		nameObj.push(name);
-
-		titles.forEach(function (e, i) {
-			let similarity = compareTwoStrings(
-				name.toLowerCase(),
-				e.name.toLowerCase()
-			);
-			if (similarity > 0.95) {
-				if (indx != i) {
-					isMatch = true;
-					// console.log(name, e.name, similarity);
-					nameObj.push(e.name);
-				}
-			}
-		});
-		// if (isMatch) {
-		// 	titleObjects.push(nameObj);
-		// } else {
-		// 	nameObj.push(name);
-			titleObjects.push(nameObj);
-		// }
-	});
-	return titleObjects;
 }
 
 function floorRandom(between) {
@@ -100,4 +49,58 @@ function floorRandom(between) {
 	} else {
 		return Math.floor(Math.random() * between);
 	}
+}
+
+function getCurrDate() {
+	var d = new Date(),
+		dformat =
+			[d.getMonth() + 1, d.getDate(), d.getFullYear()].join(":") +
+			"-" +
+			[d.getHours(), d.getMinutes(), d.getSeconds()].join(":");
+	return dformat;
+}
+
+function getCompared(parsedData) {
+	let similarObj = [];
+
+	for (let e = 0; e < 20; e++) {
+		let titleObj = {
+			title: parsedData[e].name,
+			occurences: parsedData[e].count,
+			included_in_count: 0,
+			score: 0,
+			similar: [],
+		};
+
+		parsedData.forEach(function (el, ind) {
+			if (el.name.includes(parsedData[e].name)) {
+				if (e != ind) {
+					if (titleObj.similar.length < 15) {
+						titleObj.similar.push({ name: el.name, count: el.count });
+						titleObj.included_in_count++;
+					}
+				}
+			}
+			// if not includes compare the strings and include the string if the score is big enough
+		});
+		titleObj.score = titleObj.occurences + titleObj.included_in_count * 0.8;
+		similarObj.push(titleObj);
+	}
+	return similarObj;
+}
+
+function writeTitles(res) {
+	fs.writeFile(
+		`titles_groupped/titles_groupped${getCurrDate()}.json`,
+		`${JSON.stringify(res)}`,
+		(err) => console.log(err)
+	);
+}
+
+function writeOffersFile(res) {
+	fs.writeFile(
+		`offer_files/justJoinData${getCurrDate()}.json`,
+		`${JSON.stringify(res)}`,
+		(err) => console.log(err)
+	);
 }
